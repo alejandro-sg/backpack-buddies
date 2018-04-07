987846bf91d69653d19a5874a99cac8945ebcd6f
zhik <zhikhe@gmail.com>
2018-04-07 06:30:02 -0400

    added points and circles

--- Main.js
+++ Main.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, Circle } from 'react-native-maps';
 
 const { width, height } = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
-const LATITUDE_DELTA = 0.00003; //few blocks
+const LATITUDE_DELTA = 0.0003; //few blocks
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const points = [
+  {
+    key: 1,
+    coordinate: {latitude: 38.99362310388709, longitude:-76.94091223793413},
+    title: 'test',
+    description: 'test',
+    image: 'https://cdn2.iconfinder.com/data/icons/fatcow/32x32/pizza.png',
+  },
+  {
+    key: 2,
+    coordinate: {latitude: 38.9936, longitude:-76.95},
+    title: 'test2',
+    description: 'test2',
+    image: 'https://cdn2.iconfinder.com/data/icons/fatcow/32x32/pizza.png',
+  }
+
+]
 
 export default class Main extends React.Component {
     constructor(props) {
         super(props);
     
         this.state = {
-          latitude: null,
-          longitude: null,
+          latitude: 0,
+          longitude: 0,
           error: null,
           region: {
             latitude: 0,
@@ -28,13 +46,13 @@ export default class Main extends React.Component {
 
     
     onRegionChange(region) {
-      console.log(region);
       this.setState({ region });
     }
-
+    
     componentDidMount(){
         navigator.geolocation.getCurrentPosition(
           (position) => {
+            //init region(viewport) on user position
             const region = {
               latitude: position.coords.latitude,
               longitude: position.coords.longitude, 
@@ -54,7 +72,26 @@ export default class Main extends React.Component {
         );
       }
 
+
+
     render(){
+        const circles = this.state.latitude ? 
+          <React.Fragment>
+          <Circle 
+          center={ {latitude: this.state.latitude, longitude:this.state.longitude} }
+          radius={250}
+          strokeColor='rgba(192,203,255,0.4)'
+          fillColor='rgba(192,203,255,0.4)'
+          /> 
+          <Circle 
+            center={ {latitude: this.state.latitude, longitude:this.state.longitude} }
+            radius={500}
+            strokeColor='rgba(255,102,102,0.2)'
+            fillColor='rgba(255,102,102,0.2)'
+          /> 
+          </React.Fragment>
+          : null
+
         return(
             <View>
                 <Text>{this.state.latitude}, {this.state.longitude}</Text>
@@ -62,11 +99,22 @@ export default class Main extends React.Component {
                   region={this.state.region}
                   onRegionChange={this.onRegionChange}
                 >
-                  <Marker
-                    coordinate={{latitude: 38.99362310388709, longitude:-76.94091223793413}}
-                    title="test"
-                    description="test"
+                  {
+                    points.map(point=> (
+                      <Marker 
+                        key={point.key}
+                        coordinate={point.coordinate}
+                        title={point.title}
+                        description={point.description}
+                        image={point.image}
+                      />
+                    ))
+                  }
+
+                  <Marker 
+                    coordinate= { {latitude: this.state.latitude, longitude:this.state.longitude} }
                   />
+                  {circles}
                 </MapView>
             </View>
         )
