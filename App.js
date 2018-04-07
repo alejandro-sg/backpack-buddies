58c02496a5722e976c52d459e313f280b47df134
zhik <zhikhe@gmail.com>
2018-04-07 01:26:32 -0400

    geolocation

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Map from './Map';
+
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>cool</Text>
+        <Map/>
       </View>
     );
   }
